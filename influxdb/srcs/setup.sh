223ab062ffc5c2a084f193276c01fd387d24c3b4
eliott depauw <eliott@MacBook-Pro-de-eliott.local>
2021-06-04 08:48:33 +0200

    hi

--- influxdb/srcs/setup.sh
+++ influxdb/srcs/setup.sh
@@ -1,4 +1,19 @@
-# !/bin/sh
-rc-service influxdb start
-telegraf &
-sleep infinite
+openrc
+touch /run/openrc/softlevel
+
+service influxdb start
+
+until influx; do
+	sleep 1
+done
+
+influx -execute "CREATE USER admin WITH PASSWORD '${IDB_ADMIN_PASS}' WITH ALL PRIVILEGES;"
+influx -execute "CREATE DATABASE metrics;"
+influx -execute "CREATE USER telegraf_user WITH PASSWORD '${IDB_TELEGRAF_PASS}';"
+influx -execute "GRANT ALL ON metrics TO telegraf_user;"
+influx -execute "CREATE USER grafana_user WITH PASSWORD '${IDB_GRAFANA_PASS}';"
+influx -execute "GRANT ALL ON metrics TO grafana_user;"
+
+service influxdb stop
+
+influxd -config /etc/influxdb.conf
